b11358616a969c3c823148bd8c3d31734ed08d2e
Hanh Le <jamiehanhle@gmail.com>
2024-01-31 21:38:21 -0800

    Started on viewer form

--- TournamentTracker/TrackerUI/TournamentViewerForm.Designer.cs
+++ TournamentTracker/TrackerUI/TournamentViewerForm.Designer.cs
@@ -32,9 +32,12 @@
             // 
             // TournamentViewerForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1200, 595);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = Color.White;
+            Margin = new Padding(5, 6, 5, 6);
             Name = "TournamentViewerForm";
             Text = "Tournament Viewer";
             Load += TournamentViewerForm_Load;
